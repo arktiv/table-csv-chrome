d25179524eb4e4014ddd375ea4c789022677765f
arktiv <39712769+arktiv@users.noreply.github.com>
2019-07-19 12:37:59 +1200

    Add files via upload
    change file name 
    saves url for images in table

--- Download table as CSV/downloadcsv.js
+++ Download table as CSV/downloadcsv.js
@@ -15,13 +15,14 @@
 			}
 			table = table.cloneNode(true);
 			let csv = [];
-			let rows = table.rows;			
+			let rows = table.rows;				
 			for (let i = 0; i < rows.length; i++) {
 				let row = [], cols = rows[i].querySelectorAll("td, th");
 				
 				for (let j = 0; j < cols.length; j++) {
-					let columnItem = cols[j].innerText.replace("\"", "\"\""); //as per rfc4180
-					columnItem = columnItem.replace(/(\r\n\t|\n|\r\t)/gm," "); //New lines are noting but trouble
+					let columnItem = cols[j].innerText.replace("\"", "\"\""); //as per rfc4180					
+					columnItem = columnItem.replace(/(\r\n\t|\n|\r\t)/gm," "); //New lines are nothing but trouble
+					cols[j].querySelectorAll("img").forEach(function(ele){ columnItem = columnItem + (columnItem.length > 0 ? " " : "") + ele.src; });
 					row.push("\"" + columnItem + "\"");
 					for(let a = 1; a < cols[j].colSpan; a++){
 						row.push("\"\""); //keep alignment by adding empty cells for colSpan
@@ -30,12 +31,12 @@
 						rows[i+a].insertBefore(document.createElement("td"), rows[i+a].children[j]); //keep alignment by adding empty cells for rowSpan
 					}
 				}
-				csv.push(row.join(","));  
-				
-				debugger;				
+				csv.push(row.join(","));  				
 			}  
 			let downloadLink = document.createElement("a");
-			downloadLink.download = (table.id || table.id.length > 0) ? table.id + ".csv" : "table.csv";
+			let fileName = prompt("File name: ", (table.id || table.id.length > 0) ? table.id : "table");
+			if(fileName == null) return;			
+			downloadLink.download = fileName != "" ? fileName + ".csv" : "table.csv"
 			downloadLink.href = window.URL.createObjectURL(new Blob([csv.join("\r\n")], {type: "text/csv"}));
 			downloadLink.style.display = "none";
 			document.body.appendChild(downloadLink);
