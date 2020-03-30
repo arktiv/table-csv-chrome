f6f682812fbdac2e32d982a35646b55343ed7a67
arktiv <39712769+arktiv@users.noreply.github.com>
2020-03-31 11:05:02 +1300

    Update background.js
    Updates to allow use of activeTab permission

--- Download table as CSV/background.js
+++ Download table as CSV/background.js
@@ -1,10 +1,9 @@
 chrome.runtime.onInstalled.addListener(function() {
-	chrome.contextMenus.create({ id: "DLCSV", title: "Download table as CSV", type: "normal", contexts: ["page"] });
+	chrome.contextMenus.create({ id: "DLCSV", title: "Download table as CSV", type: "normal", contexts: ["page"]});	
 });
-
 chrome.contextMenus.onClicked.addListener(function(item, tab) {
 		"use strict";
-		if(item.menuItemId == "DLCSV"){
-			chrome.tabs.sendMessage(tab.id, "downloadTableAsCSV");
-		}
+		if(item.menuItemId == "DLCSV"){	
+			chrome.tabs.executeScript(tab.id, {file: "downloadcsv.js", allFrames:true});			
+		}	
 });
