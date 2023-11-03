aef647591689fac90172895a27e3a2c534d3b9b4
ijepale@gmail.com <ijepale@gmail.com>
2023-11-03 15:30:14 +0300

    update

--- Download table as CSV/downloadcsv.js
+++ Download table as CSV/downloadcsv.js
@@ -1,54 +1,49 @@
-(()=> {
-	"use strict";
-	function startDownload(){
-		if(document.getSelection().focusNode == null)return;
-		let clickedEl = document.getSelection().focusNode.parentElement;
-		let table = clickedEl.closest("table");		
-		if(table === null){
-			alert("No HTML table was found");
-			return;
-		}
-		table = table.cloneNode(true);
-		let csv = [];
-		let rows = table.rows;				
-		for (let i = 0; i < rows.length; i++) {
-			let row = [], cols = rows[i].querySelectorAll("td, th");	
-			for (let j = 0; j < cols.length; j++) {
-				let columnItem = cols[j].innerText.replace(/"/g, "\"\""); //as per rfc4180
-				columnItem = columnItem.replace(/(\r\n\t|\n|\r\t)/gm," ").trim(); //New lines are nothing but trouble										
-				cols[j].querySelectorAll("img").forEach((ele)=>{ columnItem = columnItem + (columnItem.length > 0 ? " " : "") + ele.src; });
-				cols[j].querySelectorAll("input, textarea").forEach((ele)=>{ columnItem = columnItem + (columnItem.length > 0 ? " " : "") + ele.value + " (i)"; });
-				row.push("\"" + columnItem + "\"");
-				for(let a = 1; a < cols[j].colSpan; a++){
-					row.push("\"\""); //keep alignment by adding empty cells for colSpan
-				}
-				for(let a = 1; a < cols[j].rowSpan; a++){
-					rows[i+a].insertBefore(document.createElement("td"), rows[i+a].children[j]); //keep alignment by adding empty cells for rowSpan
-				}
-			}
-			csv.push(row.join(","));  				
-		}  
-		let downloadLink = document.createElement("a");
-		let fileName = prompt("File name: ", (table.id || table.id.length > 0) ? table.id : "table");
-		if(fileName == null) return;			
-		downloadLink.download = fileName != "" ? fileName + ".csv" : "table.csv";
-		chrome.storage.local.get(["encoding"], (result)=> {
-			let dataBlob = (result["encoding"] == "utf") ? new Blob([csv.join("\r\n")], {type: "text/csv"}) : new Blob([new Uint8Array([0xEF, 0xBB, 0xBF]), csv.join("\r\n")], {type: "text/csv;charset=utf-8"});
-			downloadLink.href = window.URL.createObjectURL(dataBlob);
-			downloadLink.style.display = "none";
-			document.body.appendChild(downloadLink);
-			downloadLink.click();	
-		});	
-	}
-	
-	if(window.hasOwnProperty("dltcsvRightClick") && dltcsvRightClick){
-		dltcsvRightClick = false;
-		startDownload();
-	}else{
-		let originalCursor = document.body.style.cursor;
-		document.addEventListener("click", (event)=>{ 
-			document.body.style.cursor = originalCursor; 
-			startDownload(); }, {once:true}, true);
-		document.body.style.cursor = "crosshair";		
-	}
+(async () => {
+    "use strict";
+
+    async function startDownload() {
+        const table = document.querySelector("#tbl");
+        
+        // Get the selected month and year
+        const month = document.querySelector("#mnth option:checked").text;
+        const year = document.querySelector("#year").value;
+
+        // Add the month and year to the CSV data
+        const csv = [`${month} - ${year}`];
+
+        const clonedTable = table.cloneNode(true);
+        clonedTable.querySelectorAll("tr").forEach((row) => {
+            const csvRow = [];
+            row.querySelectorAll("td, th").forEach((cell) => {
+                let columnItem = cell.innerText.replace(/"/g, "\"\"").replace(/(\r\n\t|\n|\r\t)/gm, " ").trim();
+                cell.querySelectorAll("img").forEach((img) => columnItem += (columnItem.length > 0 ? " " : "") + img.src);
+                cell.querySelectorAll("input, textarea").forEach((input) => columnItem += (columnItem.length > 0 ? " " : "") + input.value + " (i)");
+
+                csvRow.push(`"${columnItem}"`.repeat(cell.colSpan || 1));
+            });
+            csv.push(csvRow.join(","));
+        });
+
+        let csvData = '\n\n' + csv.join("\r\n");
+
+        // Request a file handle.
+        const [fileHandle] = await window.showOpenFilePicker();
+
+        // Request permission to write.
+        await fileHandle.requestPermission({ mode: 'readwrite' });
+
+        // Create a FileSystemWritableFileStream to write to.
+        const writableStream = await fileHandle.createWritable();
+
+        // Make sure we start with the file's current contents.
+        await writableStream.write(await fileHandle.getFile().then(file => file.text()));
+
+        // Write the new contents of the file.
+        await writableStream.write(csvData);
+
+        // Close the stream.
+        await writableStream.close();
+    }
+
+    startDownload();
 })();
